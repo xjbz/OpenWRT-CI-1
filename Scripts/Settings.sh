399faadabd6ece803191a486b8197087a8bf8d7b
xjbz <127511207+xjbz@users.noreply.github.com>
2026-03-16 09:07:41 +0000

    update

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -69,5 +69,5 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 		echo "qualcommax set up nowifi successfully!"
 	fi
 	#其他调整
-	echo "CONFIG_PACKAGE_kmod-usb-serial-qualcomm=y" >> ./.config
+	# echo "CONFIG_PACKAGE_kmod-usb-serial-qualcomm=y" >> ./.config
 fi
